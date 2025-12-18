98e3f13d3ee291fb07a015373626325d8551e44a
devdinc <234956748+devdinc@users.noreply.github.com>
2025-12-18 13:54:52 +0300

    Update PaperRoutineConfiguration.java

--- paper/src/main/java/github/devdinc/routines/config/impl/PaperRoutineConfiguration.java
+++ paper/src/main/java/github/devdinc/routines/config/impl/PaperRoutineConfiguration.java
@@ -13,28 +13,6 @@ import github.devdinc.routines.util.Scheduler;
 public class PaperRoutineConfiguration {
     public static RoutineConfiguration paperRoutineConfiguration(Plugin plugin) {
         return new RoutineConfiguration(new LogAndContinue(),
-                new SchedulingConfiguration.ALL() {
-
-                    @Override
-                    public Duration after() {
-                        return Duration.ZERO;
-                    }
-
-                    @Override
-                    public Duration every() {
-                        return Duration.ZERO;
-                    }
-
-                    @Override
-                    public Scheduler scheduler() {
-                        return new PaperScheduler(plugin, context());
-                    }
-
-                    @Override
-                    public PaperContext context() {
-                        return null;
-                    }
-
-                });
+                new PaperSchedulerConfiguration(plugin));
     }
 }
